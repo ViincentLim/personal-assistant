a787ae3a566909c36082b293deed1adcdbbb2549
Vincent Lim <vincentlimjingen@gmail.com>
2023-01-24 21:20:24 +0800

    Fix Android dependencies errors

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -25,6 +25,17 @@ android {
         versionCode = 1
         versionName = "1.0-SNAPSHOT"
     }
+    packagingOptions {
+        exclude("META-INF/DEPENDENCIES")
+        exclude("META-INF/LICENSE")
+        exclude("META-INF/LICENSE.txt")
+        exclude("META-INF/license.txt")
+        exclude("META-INF/NOTICE")
+        exclude("META-INF/NOTICE.txt")
+        exclude("META-INF/notice.txt")
+        exclude("META-INF/ASL2.0")
+        exclude("META-INF/*.kotlin_module")
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
